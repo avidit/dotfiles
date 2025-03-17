aca65b33b02bd10689b109cd2dfe43a9fbac8ae1
Aabishkar KC <aabishkar@gmail.com>
2025-03-17 16:58:45 -0400

    update windows dotfiles

--- profile.ps1
+++ profile.ps1
@@ -1,8 +1,5 @@
 # import modules
 
-# https://github.com/devblackops/Terminal-Icons
-Import-Module -Name Terminal-Icons
-
 # https://github.com/dahlbyk/posh-git
 Import-Module posh-git
 $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
@@ -31,19 +28,6 @@ $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Magenta
 $GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::Blue
 $GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::Blue
 $GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::Blue
-
-# npmrc profile
-# https://github.com/deoxxa/npmrc
-function prompt {
-    if (Get-Command npmrc -errorAction SilentlyContinue) {
-        $npmrc = Split-Path $(Get-Item $(npm get userconfig)).Target -Leaf
-        $prompt = Write-Prompt "[ $npmrc] " -ForegroundColor ([ConsoleColor]::Red)
-        $prompt += & $GitPromptScriptBlock
-        if ($prompt) { "$prompt " } else { " " }
-    } else {
-        & $GitPromptScriptBlock
-    }
-}
 
 # https://github.com/vors/ZLocation
 Import-Module ZLocation
